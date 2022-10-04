ccb40407f93016121a180dd4808307ec6d051389
jinwoooooo <song427j@gmail.com>
2022-10-04 13:44:03 +0900

    화폐 매수

---
+++ src/CurrencyCnt.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CurrencyCnt {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("금액을 넣으세요:");
+        int total = in.nextInt();
+
+        int[] Moneytype = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+
+        for (int i = 0 ; i < 8 ; i ++) {
+            int num = total / Moneytype[i];
+            total %= Moneytype[i];
+            System.out.printf("%d원%d개\n",Moneytype[i],num);
+        }
+
+
+
+
+
+        in.close();
+    }
+}
