f08caeda6302be89c5df38305d8b9d5bad5ead35
jinwoooooo <song427j@gmail.com>
2022-10-05 10:32:50 +0900

    Calculator divide 수정

---
+++ src/Week3/Calculator2.java
@@ -0,0 +1,25 @@
+package Week3;
+
+public class Calculator2 {
+
+    public void plus(int a, int b){
+        int result = a+b;
+        System.out.println(result);
+    }
+
+    public void minus(int a, int b){
+        int result = a-b;
+        System.out.println(result);
+    }
+
+    public void multiple(int a, int b){
+        int result = a*b;
+        System.out.println(result);
+    }
+
+    public void divide(int a, int b){
+        float result = (float) a/b;
+        System.out.println(result);
+    }
+
+}
