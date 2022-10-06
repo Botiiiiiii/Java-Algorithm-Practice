6474983141910a75f22252cdf827e9bbefc58980
jinwoooooo <song427j@gmail.com>
2022-10-06 15:13:23 +0900

    File list

---
+++ src/Week3/Day3/File_read_exercise/FileExercise.java
@@ -0,0 +1,13 @@
+package Week3.Day3.File_read_exercise;
+
+import java.io.File;
+
+public class FileExercise {
+    public static void main(String[] args) {
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        for (File file : files){
+            System.out.println(file);
+        }
+    }
+}
