8009f7255922f4db546e99b545d5ed2f1dc54a70
jinwoooooo <song427j@gmail.com>
2022-10-21 10:45:20 +0900

    스택으로 괄호문제 풀기

---
+++ src/stack/StackExercise.java
@@ -0,0 +1,27 @@
+package stack;
+
+import java.util.Stack;
+
+public class StackExercise {
+
+    public boolean solution(String s){ // 스택없이 문제 풀기
+        while (s.indexOf("()") >= 0) {
+            s = s.replace("()","");
+        }
+        return s.length() == 0;
+    }
+
+    public boolean solution_stack(String s){ // 스택으로 문제 풀기
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i<s.length(); i++){
+            if ('(' == s.charAt(i)) {
+                st.push(s.charAt(i));
+            } else if (')' == s.charAt(i)){
+                if(st.empty()) return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+
+}
