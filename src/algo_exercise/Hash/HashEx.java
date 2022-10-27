1d7c45c3a3c67cdcffc0b4aea5587b48cc890eea
jinwoooooo <song427j@gmail.com>
2022-10-27 09:44:10 +0900

    programmers_완주하지못한선수

--- src/algo_exercise/Hash/HashEx.java
+++ src/algo_exercise/Hash/HashEx.java
@@ -1,25 +1,38 @@
 package algo_exercise.Hash;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class HashEx {
     public String Solution(String[] participant, String[] completion){
         HashMap<String, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < participant.length; i++) {
-            if (hashMap.get(participant[i]) == null){
-                hashMap.put(participant[i],1);
+            if (hashMap.containsKey(participant[i])){
+                hashMap.put(participant[i],hashMap.get(participant[i]) + 1);
             } else{
-              int tmp = hashMap.get(participant[i]);
-              hashMap.put(participant[i],tmp + 1);
+                hashMap.put(participant[i],1);
             }
         }
 
         // for 문을 통해 있는 값 1씩 빼기
+        for (int i = 0; i < completion.length; i++) {
+            if (hashMap.get(completion[i]) == 1){
+                hashMap.remove(completion[i]);
+            }
+            else{
+                hashMap.put(completion[i],hashMap.get(completion[i]) - 1);
+            }
+        }
+        Object[] answer = hashMap.keySet().toArray();
+
+        return answer[0].toString();
 
     }
     public static void main(String[] args) {
-
-
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        HashEx hashEx = new HashEx();
+        System.out.println(hashEx.Solution(participant, completion));
     }
 
 }
