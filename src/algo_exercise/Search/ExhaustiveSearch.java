08326f1b1b127d85bf4562d239255bd79e481db0
jinwoooooo <song427j@gmail.com>
2022-10-31 10:31:52 +0900

    프로그래머스 모의고사 ([1,0,0] 해결중)

--- src/algo_exercise/Search/ExhaustiveSearch.java
+++ src/algo_exercise/Search/ExhaustiveSearch.java
@@ -2,18 +2,39 @@ package algo_exercise.Search;
 
 public class ExhaustiveSearch {
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        int tmp = 1;
-        int fistStudent = 0;
+        int[] answer = new int[3];
+        int[] firstStudent = {1,2,3,4,5};
+        int[] secondStudent = {2,1,2,3,2,4,2,5};
+        int[] thirdStudent = {3,3,1,1,2,2,4,4,5,5};
+        int[] cnt = {0,0,0};
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == tmp%6){
-                fistStudent += 1;
+            if (answers[i] == firstStudent[i%5]){
+                cnt[0] += 1;
             }
-            tmp++;
         }
-
-        System.out.println(fistStudent);
-
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == secondStudent[i%8]){
+                cnt[1] += 1;
+            }
+        }
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == thirdStudent[i % 10]) {
+                cnt[2] += 1;
+            }
+        }
+        int max = cnt[2];
+        for (int i = 0; i < 2; i++) {
+            if(cnt[i] > max){
+                max = cnt[i];
+            }
+        }
+        int tmp = 0;
+        for (int i = 0; i < 3; i++) {
+            if (max == cnt[i]){
+                answer[tmp] = i+1;
+                tmp ++;
+            }
+        }
 
         return answer;
     }
