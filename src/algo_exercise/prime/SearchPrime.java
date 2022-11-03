fe807db1266a0eacefddc274a09fc1315098131f
jinwoooooo <song427j@gmail.com>
2022-11-03 10:05:37 +0900

    문제 풀기 전 세팅

--- src/algo_exercise/prime/SearchPrime.java
+++ src/algo_exercise/prime/SearchPrime.java
@@ -1,17 +1,21 @@
 package algo_exercise.prime;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SearchPrime {
     public boolean Solution(int num){
-        List<Integer> nums = new ArrayList<>();
-        for (int i = 0; i < num; i++) {
-            nums.add(i);
-        }
+        int N = 50; // N = 50
+        int[] nums = new int[N-1]; // N-1개만큼 2~50까지 총 49개
+        boolean[] checks = new boolean[nums.length]; // nums만큼 checks생성
+        Arrays.fill(checks, true); // checks를 true로 초기화 cuz 기본값 false
+        for (int i = 0; i < nums.length; i++) nums[i] = i + 2; // 2~50까지 nums에 채우기
+        return true;
+    }
 
-        for (int i = 2; i < nums.size(); i++) {
-            if(nums.get(i) % 2 == 0 )
-        }
+    public static void main(String[] args) {
+        SearchPrime searchPrime = new SearchPrime();
+        searchPrime.Solution(10);
     }
 }
