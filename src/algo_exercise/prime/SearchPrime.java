d23e4ea20a98e964a6094d55ff37685b3cc40715
jinwoooooo <song427j@gmail.com>
2022-11-03 09:24:35 +0900

    에라토스테네스 체 실습 (진행중)

---
+++ src/algo_exercise/prime/SearchPrime.java
@@ -0,0 +1,17 @@
+package algo_exercise.prime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchPrime {
+    public boolean Solution(int num){
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            nums.add(i);
+        }
+
+        for (int i = 2; i < nums.size(); i++) {
+            if(nums.get(i) % 2 == 0 )
+        }
+    }
+}
