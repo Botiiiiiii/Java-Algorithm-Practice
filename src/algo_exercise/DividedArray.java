9f3931e328f3386b8d703e6e1c5d1e2c9f5a6391
jinwoooooo <song427j@gmail.com>
2022-11-07 10:31:55 +0900

    나누어 떨어지는 숫자 완료

--- src/algo_exercise/DividedArray.java
+++ src/algo_exercise/DividedArray.java
@@ -2,25 +2,31 @@ package algo_exercise;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class DividedArray {
     public int[] solution(int[] arr, int divisor) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        Arrays.sort(arr);
+
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % divisor == 0) answer.add(arr[i]);
-        }
-        int[] answer_list = new int[answer.size()];
-
-        for (int i = 0; i < answer_list.length; i++) {
-            answer_list[i] = answer.get(i);
+            if (arr[i] % divisor == 0) list.add(arr[i]);
         }
 
-        return answer_list;
+        if(list.size() == 0) return new int[]{-1};
+
+        // list를 Array로 바꾸고
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        // sort 한 것을 리턴
+        Arrays.sort(answer);
+        return answer;
     }
 
     public static void main(String[] args) {
         DividedArray dividedArray = new DividedArray();
-        System.out.println(dividedArray.solution([5, 9, 7, 10],5));
+        int[] arr = {5,9,7,10};
+        System.out.println(Arrays.toString(dividedArray.solution(arr, 5)));
     }
 }
