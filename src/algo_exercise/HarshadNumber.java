4db8463678fc4ddd31f89d959423c82d015a7d32
jinwoooooo <song427j@gmail.com>
2022-11-07 09:31:22 +0900

    하샤드 수 완료

--- src/algo_exercise/HarshadNumber.java
+++ src/algo_exercise/HarshadNumber.java
@@ -3,16 +3,17 @@ package algo_exercise;
 public class HarshadNumber {
     public boolean solution(int x) {
         int sum = 0;
-        while(x != 0){
-            sum += x%10;
-            x /= 10;
+        int tmp;
+        tmp = x;
+        while(tmp != 0){
+            sum += tmp%10;
+            tmp /= 10;
         }
-        System.out.println(sum);
-        return true;
+        return x%sum == 0;
     }
 
     public static void main(String[] args) {
         HarshadNumber harshadNumber = new HarshadNumber();
-        harshadNumber.solution(10);
+        System.out.println(harshadNumber.solution(11));
     }
 }
