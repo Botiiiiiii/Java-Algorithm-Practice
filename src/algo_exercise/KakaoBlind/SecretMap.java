2874ba13d99ffaf6487fb8972c56497d0935990d
jinwoooooo <song427j@gmail.com>
2022-11-11 09:42:02 +0900

    비밀지도 10진수 to 2진수

--- src/algo_exercise/KakaoBlind/SecretMap.java
+++ src/algo_exercise/KakaoBlind/SecretMap.java
@@ -1,8 +1,23 @@
 package algo_exercise.KakaoBlind;
 
+
 public class SecretMap {
-    // arr1 해독
-    // arr2 해독
-    // 해독된 arr1과 arr2 비교
-    // 비교된 배열 값 출력
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[]{"123","123"};
+        String arr1_list = "";
+        while(arr1[0] > 0) {
+            arr1_list = arr1[0] % 2 + arr1_list;
+            arr1[0] /= 2;
+        }
+        System.out.println(arr1_list);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        SecretMap secretMap = new SecretMap();
+        int n = 5;
+        int[] arr1 = new int[]{9,29,28,18,11};
+        int[] arr2 = new int[]{30,1,21,17,28};
+        secretMap.solution(n,arr1,arr2);
+    }
 }
