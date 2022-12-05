e0390daef174b9a89a7d10cc494fa03ed132b6b7
jinwoooooo <song427j@gmail.com>
2022-12-05 09:28:58 +0900

    배열 생성 후, 자리 변경 (6,5,7,8)

---
+++ src/algo_exercise/Heap/MakeHeap.java
@@ -0,0 +1,24 @@
+package algo_exercise.Heap;
+
+import java.util.Arrays;
+
+public class MakeHeap {
+    public static void main(String[] args) {
+        int[] arr = {6,5,7,8};
+        System.out.println(Arrays.toString(arr));
+
+
+        // 8과 5
+        int tmp = arr[1];
+        arr[1] = arr[3];
+        arr[3] = tmp;
+
+        //8과 6
+        tmp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = tmp;
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
